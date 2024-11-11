b52549c48cb3a4eb5bd4939abe8118308be00f2d
mdos-san <mdossan@protonmail.com>
2024-11-11 03:26:33 +0100

    feat: implement `DatabaseProviderAbstract`

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,18 +1,41 @@
 import { DatabaseEntity } from "@mds-coding/database-entity";
 import { MongoDBContainer } from "@testcontainers/mongodb";
 import { DatabaseMongo } from "./index.js";
+import { DatabaseQuery } from "@mds-coding/database-query";
+import { DatabaseCriteria } from "@mds-coding/database-criteria";
+
+type Cat = { id: string, name: string, age: number, cute: boolean };
 
 test("`DatabaseMongo` can insert and findById", async () => {
   const container = await new MongoDBContainer().start();
   const database = new DatabaseMongo(`${container.getConnectionString()}?directConnection=true`, "database");
   try {
-    await database.insert(new DatabaseEntity("123", "collection", { hello: "world" }));
-    const document = await database.findById<{ id: string, hello: string }>("collection", "123");
+    await database.connect();
+
+    // Insert 1
+    await database.insert(new DatabaseEntity<Cat>("plume", "cats", { id: "plume", name: "Plume", age: 5, cute: false }));
+    // Insert 2
+    await database.insert(new DatabaseEntity<Cat>("scratchy", "cats", { id: "scratchy", name: "Scratchy", age: 10, cute: false }));
+
+    // Find all
+    const document = await database.findMany<Cat>(new DatabaseQuery("cats", []));
     expect(document).toBeDefined();
-    expect(document.id).toBe("123");
-    expect(document.collection).toBe("collection");
-    expect(document.data.id).toBe("123");
-    expect(document.data.hello).toStrictEqual("world");
+    expect(document.length).toBe(2);
+
+    // Find all Plume
+    const documentPlume = await database.findMany<Cat>(new DatabaseQuery("cats", [ new DatabaseCriteria<Cat, "name">("name", 'EQ', 'Plume')]));
+    expect(documentPlume).toBeDefined();
+    expect(documentPlume.length).toBe(1);
+    expect(documentPlume[0].age).toBe(5);
+
+    // Update many
+    await database.updateMany<Cat>(new DatabaseQuery("cats", []), { cute: true });
+
+    // Find one scratchy
+    const scratchyOne = await database.findOne<Cat>(new DatabaseQuery("cats", [ new DatabaseCriteria<Cat, "name">("name", 'EQ', 'Scratchy')]));
+    expect(scratchyOne).toBeDefined();
+    expect(scratchyOne.age).toBe(10);
+    expect(scratchyOne.cute).toBe(true);
   } finally {
     await database.disconnect();
     await container.stop();
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,99 @@
+import { DatabaseCriteria } from "@mds-coding/database-criteria";
 import { DatabaseEntity } from "@mds-coding/database-entity";
-import { Db, Document, MongoClient } from "mongodb";
+import { DatabaseProviderAbstract } from "@mds-coding/database-provider-abstract";
+import { DatabaseQuery } from "@mds-coding/database-query";
+import { Db, MongoClient } from "mongodb";
 
-export class DatabaseMongo {
-  client: MongoClient;
-  database: Db;
+export class DatabaseMongo extends DatabaseProviderAbstract {
+  client: MongoClient = {} as MongoClient;
+  database: Db = {} as Db;
 
-  constructor(connectionString: string, databaseName: string) {
-    this.client = new MongoClient(connectionString);
-    this.database = this.client.db(databaseName);
+  constructor(private connectionString: string, private databaseName: string) {
+    super();
+  }
+
+  async connect(): Promise<void> {
+    this.client = new MongoClient(this.connectionString);
+    this.database = this.client.db(this.databaseName);
+    this.client.connect
   }
 
   async disconnect() {
     return this.client.close()
   }
 
-  async insert<T extends Document>(entity: DatabaseEntity<T>) {
+  async insert<T>(entity: DatabaseEntity<T>) {
     const collection = this.database.collection(entity.collection);
     await collection.insertOne({ id: entity.id, ...entity.data });
-    return entity;
+    return entity.data;
   }
 
-  async findById<T>(collectionName: string, id: string) {
+  async updateOne<T>(query: DatabaseQuery<T>, fieldsToUpdate: Partial<T>): Promise<T> {
+    const { collection: collectionName } = query;
     const collection = this.database.collection(collectionName);
-    const document = await collection.findOne({ id: id });
-
+    const finalQuery = query.criterias.map(criteriaToMongo).reduce((acc, current) => {
+      return { ...acc, ...current }
+    }, {});
+    const document = await collection.updateOne(finalQuery, { $set: fieldsToUpdate });
     if (!document) {
-      throw new Error(`Document ${id} not found id collection ${collectionName}`)
+      throw new Error(`No document found`)
     }
-
-    console.log({ document })
-    return new DatabaseEntity<T>(id, collectionName, document as T);
+    return document as T;
   }
+
+  async updateMany<T>(query: DatabaseQuery<T>, fieldsToUpdate: Partial<T>): Promise<T[]> {
+    const { collection: collectionName } = query;
+    const collection = this.database.collection(collectionName);
+    const finalQuery = query.criterias.map(criteriaToMongo).reduce((acc, current) => {
+      return { ...acc, ...current }
+    }, {});
+    const document = await collection.updateMany(finalQuery, { $set: fieldsToUpdate });
+    if (!document) {
+      throw new Error(`No document found`)
+    }
+    return [] as T[];
+  }
+
+  async findOne<T>(query: DatabaseQuery<T>): Promise<T> {
+    const { collection: collectionName } = query;
+    const collection = this.database.collection(collectionName);
+    const finalQuery = query.criterias.map(criteriaToMongo).reduce((acc, current) => {
+      return { ...acc, ...current }
+    }, {});
+    const document = await collection.findOne(finalQuery);
+    if (!document) {
+      throw new Error(`No document found`)
+    }
+    return document as T;
+  }
+
+  async findMany<T>(query: DatabaseQuery<T>): Promise<T[]> {
+    const { collection: collectionName } = query;
+    const collection = this.database.collection(collectionName);
+    const finalQuery = query.criterias.map(criteriaToMongo).reduce((acc, current) => {
+      return { ...acc, ...current }
+    }, {});
+    const document = await collection.find(finalQuery).toArray();
+    if (!document) {
+      throw new Error(`No documents found`)
+    }
+    return document as T[];
+  }
+}
+
+function criteriaToMongo<T>(criteria: DatabaseCriteria<T>): Object {
+  let value: any = criteria.value;
+  switch (criteria.comparator) {
+    case "LT":
+      value = { $lt: criteria.value };
+    case "LTE":
+      value = { $lte: criteria.value };
+    case "GT":
+      value = { $gt: criteria.value };
+    case "GTE":
+      value = { $gte: criteria.value };
+    case "EQ":
+      value = criteria.value;
+  }
+  return { [criteria.field]: value }
 }
